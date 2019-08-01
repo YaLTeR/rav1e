8159274c032a82b0a9ee2c32b4783fee5011ec44
Ivan Molodetskikh <yalterz@gmail.com>
2019-08-01 10:33:49 -0700

    rustfmt segmentation

--- src/segmentation.rs
+++ src/segmentation.rs
@@ -10,47 +10,49 @@
 #![allow(safe_extern_statics)]
 
 use crate::context::*;
+use crate::header::PRIMARY_REF_NONE;
+use crate::util::Pixel;
 use crate::FrameInvariants;
 use crate::FrameState;
-use crate::util::Pixel;
-use crate::header::PRIMARY_REF_NONE;
 
-pub fn segmentation_optimize<T: Pixel>(fi: &FrameInvariants<T>, fs: &mut FrameState<T>) {
-    // Segmentation is currently used only for the quantizer RDO.
-    if fi.config.speed_settings.quantizer_rdo {
-      fs.segmentation.enabled = true;
-      fs.segmentation.update_map = true;
+pub fn segmentation_optimize<T: Pixel>(
+  fi: &FrameInvariants<T>, fs: &mut FrameState<T>,
+) {
+  // Segmentation is currently used only for the quantizer RDO.
+  if fi.config.speed_settings.quantizer_rdo {
+    fs.segmentation.enabled = true;
+    fs.segmentation.update_map = true;
 
-      // We don't change the values between frames.
-      fs.segmentation.update_data = fi.primary_ref_frame == PRIMARY_REF_NONE;
+    // We don't change the values between frames.
+    fs.segmentation.update_data = fi.primary_ref_frame == PRIMARY_REF_NONE;
 
-      let TEMPORAL_RDO_QI_DELTA: i16 = fi.config.segmentation_qi_delta as i16;
+    let TEMPORAL_RDO_QI_DELTA: i16 = fi.config.segmentation_qi_delta as i16;
 
-      // Fill in 3 slots with 0, delta, -delta.
-      for i in 0..3 {
-        fs.segmentation.features[i][SegLvl::SEG_LVL_ALT_Q as usize] = true;
-        fs.segmentation.data[i][SegLvl::SEG_LVL_ALT_Q as usize] = match i {
-          0 => 0,
-          1 => TEMPORAL_RDO_QI_DELTA,
-          2 => -TEMPORAL_RDO_QI_DELTA,
-          _ => unreachable!()
-        };
+    // Fill in 3 slots with 0, delta, -delta.
+    for i in 0..3 {
+      fs.segmentation.features[i][SegLvl::SEG_LVL_ALT_Q as usize] = true;
+      fs.segmentation.data[i][SegLvl::SEG_LVL_ALT_Q as usize] = match i {
+        0 => 0,
+        1 => TEMPORAL_RDO_QI_DELTA,
+        2 => -TEMPORAL_RDO_QI_DELTA,
+        _ => unreachable!(),
+      };
+    }
+  }
+
+  /* Figure out parameters */
+  fs.segmentation.preskip = false;
+  fs.segmentation.last_active_segid = 0;
+  if fs.segmentation.enabled {
+    for i in 0..8 {
+      for j in 0..SegLvl::SEG_LVL_MAX as usize {
+        if fs.segmentation.features[i][j] {
+          fs.segmentation.last_active_segid = i as u8;
+          if j >= SegLvl::SEG_LVL_REF_FRAME as usize {
+            fs.segmentation.preskip = true;
+          }
+        }
       }
     }
-
-    /* Figure out parameters */
-    fs.segmentation.preskip = false;
-    fs.segmentation.last_active_segid = 0;
-    if fs.segmentation.enabled {
-        for i in 0..8 {
-            for j in 0..SegLvl::SEG_LVL_MAX as usize {
-                if fs.segmentation.features[i][j] {
-                    fs.segmentation.last_active_segid = i as u8;
-                    if j >= SegLvl::SEG_LVL_REF_FRAME as usize {
-                        fs.segmentation.preskip = true;
-                    }
-                }
-            }
-        }
-    }
+  }
 }
